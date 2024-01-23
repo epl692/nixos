84be688ad12e495fe160a4f908ca5a207e6c4db7
Eric Lynema <elynema@gmail.com>
2024-01-23 11:06:32 -0800

    Updates, added software

--- scripts/epl692.nix
+++ scripts/epl692.nix
@@ -1,9 +1,34 @@
   { config, pkgs, ... }:
   {
   environment.systemPackages = with pkgs; [
-    neovim
-    git
+    # Desktop applications
     brave
     threema-desktop
+
+    # System Utilies
+    wget
+    curl
+    git
+
   ];
+  
+programs.neovim = {
+  enable = true;
+  viAlias = true;
+  vimAlias = true;
+};
+
+system.autoUpgrade = {
+ enable = true;
+ dates = "*-*-* 04:00:00";
+ persistent = true;
+ allowReboot = true;
+};
+
+nix.gc = {
+ automatic = true;
+ persistent = false;
+ dates = "daily";
+ options = "--delete-older-than 7d";
+};
   }
