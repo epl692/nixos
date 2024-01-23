e9844268295764933fce545f522cd5e1e94fae81
Eric Lynema <elynema@gmail.com>
2024-01-23 11:42:53 -0500

    Initial Commit of Configuration and rebuild script.

---
+++ rebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo cp /etc/nixos/configuration.nix /etc/nixos/configuration.old
+sudo rm -r /etc/nixos-scripts/
+sudo mkdir /etc/nixos-scripts/
+sudo cp -r ./scripts/ /etc/nixos-scripts/
+sudo cp ./configuration.nix /etc/nixos/
+
+sudo nixos-rebuild switch
