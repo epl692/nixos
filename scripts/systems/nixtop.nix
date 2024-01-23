0ec8ea89d20a8c64a20ed786835294ec24678164
Eric Lynema <elynema@gmail.com>
2024-01-23 12:32:54 -0800

    fixing nixtop's bootloader

--- scripts/systems/nixtop.nix
+++ scripts/systems/nixtop.nix
@@ -3,9 +3,11 @@
   networking.hostName = "nixtop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  grub = {
-     enabled = true;
-     useOSProber = true;
-     device = "/dev/sda";
-  };
+    boot.loader = {
+      grub = {
+      enabled = true;
+      useOSProber = true;
+       device = "/dev/sda";
+      };
+    };
   }
