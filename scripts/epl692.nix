8b01e15eb85973ef91b51fb23064050288f78d77
Eric Lynema <elynema@gmail.com>
2024-01-23 15:52:15 -0800

    flakes enabled

--- scripts/epl692.nix
+++ scripts/epl692.nix
@@ -1,34 +1,40 @@
   { config, pkgs, ... }:
   {
-  environment.systemPackages = with pkgs; [
-    # Desktop applications
-    brave
-    threema-desktop
+    environment.systemPackages = with pkgs; [
+      # Desktop applications
+      brave
+      threema-desktop
 
-    # System Utilies
-    wget
-    curl
-    git
-    htop
-  ];
+      # System Utilies
+      wget
+      curl
+      git
+      htop
+    ];
   
-programs.neovim = {
-  enable = true;
-  viAlias = true;
-  vimAlias = true;
-};
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
 
-system.autoUpgrade = {
- enable = true;
- dates = "*-*-* 04:00:00";
- persistent = true;
- allowReboot = true;
-};
+  # Auto Upgrade
+  system.autoUpgrade = {
+   enable = true;
+   dates = "*-*-* 04:00:00";
+   persistent = true;
+   allowReboot = true;
+  };
 
-nix.gc = {
- automatic = true;
- persistent = false;
- dates = "daily";
- options = "--delete-older-than 7d";
-};
-  }
+  # Garbage Collector
+  nix.gc = {
+   automatic = true;
+   persistent = false;
+   dates = "daily";
+   options = "--delete-older-than 7d";
+  };
+
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+}
