79e43f34d4e2d908e0152dd37b15981bdce58a94
Eric Lynema <elynema@gmail.com>
2024-01-23 18:14:00 -0800

    well... at least I found it now.

--- prep.nix
+++ prep.nix
@@ -2,9 +2,9 @@
 { config, pkgs, ... }:
 
 {
-#  imports =
-#    [ # Include the results of the hardware scan.
-#      ./hardware-configuration.nix
-#      ./system-configuration.nix
-#    ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./system-configuration.nix
+    ];
 }
