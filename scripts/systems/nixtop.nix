3119a9e4ce7b1ffc64767f138c149c24260129a8
Eric Lynema <elynema@gmail.com>
2024-01-23 18:47:58 -0800

    typo typo typo

--- scripts/systems/nixtop.nix
+++ scripts/systems/nixtop.nix
@@ -1,13 +1,4 @@
   { config, pkgs, ... }:
   {
-  networking.hostName = "nixtop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-    boot.loader = {
-      grub = {
-      enable = true;
-      useOSProber = true;
-      devices = ["/dev/sda"];
-      };
-    };
   }
