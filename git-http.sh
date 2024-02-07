7ae15af0039d06bfacac2883ed496363f7e6cd97
Eric Lynema <elynema@gmail.com>
2024-02-06 22:44:22 -0500

    New script

---
+++ git-http.sh
@@ -0,0 +1,2 @@
+#!/run/current-system/sw/bin/bash
+git remote set-url origin https://github.com/epl692/nixos.git
---
+++ git-ssh.sh
@@ -0,0 +1,2 @@
+#!/run/current-system/sw/bin/bash
+git remote set-url origin git@github.com:epl692/nixos.git
