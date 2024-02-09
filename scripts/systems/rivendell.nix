591ca7fc6c5be19d4ce56db36870a61242df5170
Eric Lynema <elynema@gmail.com>
2024-02-08 22:23:28 -0800

    git lfs

--- scripts/systems/rivendell.nix
+++ scripts/systems/rivendell.nix
@@ -3,6 +3,7 @@
       environment.systemPackages = with pkgs; [
         #Custom to rivendell
         ollama
+        git-lfs
       ];
     networking.firewall.enable = false;
     networking.hostName = "rivendell"; # Define your hostname.
