b659df753f4f4d720f7309fd8a498c7aa567fe3b
Eric Lynema <elynema@gmail.com>
2024-02-09 20:32:29 -0800

    check again?

--- git-switch.sh
+++
@@ -1,2 +0,0 @@
-#!/run/current-system/sw/bin/bash
-git remote set-url origin git@github.com:epl692/nixos.git
--- scripts/gui.nix
+++ scripts/gui.nix
@@ -32,11 +32,7 @@ services.xserver.desktopManager.gnome = {
         };
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-#  services.xserver.displayManager.sddm.enable = true;
-#  services.xserver.desktopManager.plasma5.enable = true;
-
+  # gnome
    services.xserver.displayManager.gdm.enable = true;
    services.xserver.desktopManager.gnome.enable = true;
 
