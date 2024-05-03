cba2d8c65aec55185ddb844c692b3d02747674d1
Eric Lynema <elynema@gmail.com>
2024-05-02 20:26:12 -0700

    try again??

--- scripts/systems/nixtop.nix
+++ scripts/systems/nixtop.nix
@@ -2,5 +2,6 @@
   {
     networking.hostName = "nixtop"; # Define your hostname.
     hardware.bluetooth.enable = true;
-
+    programs.steam.enable = true;
+    nixpkgs.config.allowNonFree = true;
   }
