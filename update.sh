b7118b061978211c1230a26fc640fddd9467b887
Eric Lynema <elynema@gmail.com>
2024-05-04 13:37:38 -0700

    update update script....

--- update.sh
+++ update.sh
@@ -6,6 +6,12 @@ if test -d /etc/nixos/scripts; then
 	echo "Erase Scripts..."
 	sudo rm -r /etc/nixos/scripts/
 fi
+
+if test -d /etc/nixos/pkgs; then
+	echo "Erase Packages..."
+	sudo rm -r /etc/nixos/pkgs/
+fi
+
 if ! test -d /opt/my-wallpaper/; then
 	sudo mkdir /opt/my-wallpaper
 fi
@@ -14,6 +20,8 @@ sudo cp -r ./wallpaper.jpg /opt/my-wallpaper/wallpaper.jpg
 sudo cp -r ./wallpaper2.jpg /opt/my-wallpaper/wallpaper2.jpg
 echo "Copy Scripts..."
 sudo cp -r ./scripts /etc/nixos
+echo "Copy Packages..."
+sudo cp -r ./pkgs /etc/nixos
 echo "Copy Flake..."
 sudo cp ./flake.nix /etc/nixos
 echo "Copy Home-Manager Config..."
