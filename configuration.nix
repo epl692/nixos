eb66276f1994265a9d93981a7540eef0d7448eba
Eric Lynema <elynema@gmail.com>
2024-05-04 13:10:47 -0700

    switched to unstable. Here we go!

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "unstable"; # Did you read the comment? ((started "at 23.11"))
 
   # Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
