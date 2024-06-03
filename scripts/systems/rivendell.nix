f8d6fe87b3c0af30bafc8154dc86961799380e3c
Eric Lynema <elynema@gmail.com>
2024-06-03 15:19:40 -0700

    adding ollama service to rivendell

--- scripts/systems/rivendell.nix
+++ scripts/systems/rivendell.nix
@@ -5,4 +5,14 @@
       ];
     networking.firewall.enable = false;
     networking.hostName = "rivendell"; # Define your hostname.
+
+  systemd.user.services.ollama = {
+    description = "Ollama";
+    serviceConfig.PassEnvironment = "DISPLAY";
+    script = ''
+      OLLAMA_HOST=0.0.0.0:11434 ollama serve
+    '';
+    wantedBy = [ "multi-user.target" ]; # starts after login
   }
+
+}
